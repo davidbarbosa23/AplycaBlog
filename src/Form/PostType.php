c6380d08bba1ced8ba02e62ee19cb89dfab824d5
David Barbosa <davidbarbosa.dev@gmail.com>
2020-09-12 22:42:53 -0500

    File upload

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,24 +4,52 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('image', FileType::class)
-            ->add('content', TextareaType::class)
-            // ->add('createdAt')
-            // ->add('updatedAt')
-            // ->add('author')
-            ->add('add', SubmitType::class)
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'translation_domain' => 'forms'
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'translation_domain' => 'forms',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Content',
+                'translation_domain' => 'forms'
+            ])
+            ->add('extract', TextareaType::class, [
+                'label' => 'Extract',
+                'translation_domain' => 'forms',
+                'required' => false
+            ])
+            ->add('add', SubmitType::class, [
+                'label' => 'Add Post',
+                'translation_domain' => 'forms'
+            ])
         ;
     }
 
