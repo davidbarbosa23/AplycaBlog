74c9c83fb53b0b231bdacf1c3f257f6fb295e02a
David Barbosa <davidbarbosa.dev@gmail.com>
2020-09-14 13:57:35 -0500

    Add edit button | Adjust edit options

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,5 @@
-// $('.dropdown-toggle').dropdown();
+document.querySelector('.custom-file-input').addEventListener('change',function(e){
+    var fileName = document.getElementById("post_image").files[0].name;
+    var nextSibling = e.target.nextElementSibling
+    nextSibling.innerText = fileName
+})
